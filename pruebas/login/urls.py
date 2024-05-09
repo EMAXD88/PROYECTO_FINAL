3351c2e0fe595c8a7fa52401bb790114b76ba6b9
EMAXD88 <emmanuelbalbuena164@gmail.com>
2024-05-09 13:04:33 -0600

    ULTIMO
    YA IJA

--- pruebas/login/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.custom_login, name='login'),  # URL para la vista de login
-]
--- pruebas/subir_archivo_e_imagen/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SubirArchivoEImagenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'subir_archivo_e_imagen'
